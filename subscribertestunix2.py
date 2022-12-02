9ed55a373b33bf2a2e99770a96fa9a9341d6da65
utyrs <119175119+utyrs@users.noreply.github.com>
2022-12-02 14:09:31 +0100

    Update and rename subscribertestunix1.py to subscribertestunix2.py

--- subscribertestunix2.py
+++ subscribertestunix2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import rospy
-from visualization_msgs.msg import MarkerArray
+from rosgraph_msgs import Clock
 
 def callback(data):
     rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.data)
@@ -9,7 +9,7 @@ def callback(data):
 
 def listener():
     rospy.init_node('listener', anonymous=True)
-    rospy.Subscriber('/visualization/planning/planner_visualization', MarkerArray, callback)
+    rospy.Subscriber("/clock", Clock, callback)
     rospy.spin
 
 
