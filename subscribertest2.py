6f7f4df9d011fc96149a5009c46be870a7558307
utyrs <119175119+utyrs@users.noreply.github.com>
2022-12-02 12:00:45 +0100

    Update and rename subscribertest1.py to subscribertest2.py

--- subscribertest2.py
+++ subscribertest2.py
@@ -1,12 +1,12 @@
+#!/usr/bin/env python
 import rospy
-from std_msgs.msg import String
 from visualization_msgs.msg import MarkerArray
 
 
 def callback(data):
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
 
-    
+
 def listener():
     # In ROS, nodes are uniquely named. If two nodes with the same
     # name are launched, the previous one is kicked off. The
