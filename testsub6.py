81ec48819d6db91977d44fb474c156a22c3aca4e
utyrs <119175119+utyrs@users.noreply.github.com>
2022-12-02 18:17:47 +0100

    Add files via upload

---
+++ testsub6.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import rospy
+from visualization_msgs.msg import MarkerArray
+from visualization_msgs.msg import Marker
+
+
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() + 'I heard' + data.markers[0].pose)
+
+
+def listener():
+    rospy.init_node('listener', anonymous=True)
+    rospy.Subscriber('/visualization/planning/planner_visualization', MarkerArray, callback)
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    listener()
+
